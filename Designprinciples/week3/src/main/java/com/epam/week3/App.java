52bc7f6e7767b22449ee3227d33816496450474e
bhanu <bhanutejaswini.marella@gmail.com>
2020-07-22 16:31:34 +0530

    First commit

---
+++ Designprinciples/week3/src/main/java/com/epam/week3/App.java
@@ -0,0 +1,10 @@
+package com.epam.week3;
+//Java program to create a simple calculator 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+//with basic +, -, /, * using java swing elements 
